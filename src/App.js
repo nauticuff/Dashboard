a9bf33dfd2351b43d02776d9d00ba387a7799bf6
nauticuff <felipecarlos200x@gmail.com>
2023-03-15 23:07:11 -0700

    End of Day 1 push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import Nav from './Components/nav';
+import MainDash from './Components/maindash';
+import SubDash from './Components/subdashboard';
 
 function App() {
+  const [theme, setTheme] = useState('light')
+  
   return (
-    <div className='app light'>
-      <div className='wrapper max-w-[1440px] mx-auto'>
+    <div className={`app ${theme} flex items-center`}>
+      <div className='wrapper max-w-[1440px] mx-auto px-7'>
         <Nav/>
+        <MainDash />
+        <SubDash />
       </div>
     </div> 
   );
--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,20 +1,22 @@
 import { useState, useEffect } from "react";
 
 const Nav = () => {
-    const [theme, setTheme] = useState('light');
-    
+    //const [theme, setTheme] = useState('light');
+
     return (
-        
-        <div className="flex justify-between items-center p-8">
-            <div>
+        <div className="flex flex-col sm:flex-row sm:items-center justify-between py-7 lg:pt-0">
+            <div className="flex flex-col">
                 <h1>Social Media Dashboard</h1>
-                <p>This is small</p>
+                <p className="font-bold text-[var(--l-text-light)]">Total Followers: 23,004</p>
             </div>
+            <div className="h-[1px] bg-zinc-400 w-full my-5 sm:hidden"></div>
             <div>
-            <div className="flex items-center gap-2">
-                <p>Dark Mode</p>
-                <input className="appearance-none relative w-12 h-7 rounded-3xl cursor-pointer bg-[var(--d-toggle)] after:absolute after:top-1/2 after:left-[30%] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-[50%] after:h-5 after:w-5 after:bg-white duration-300 checked:bg-[var(--l-toggle)] checked:after:translate-x-[45%] after:transition-transform" type="checkbox"></input>
-            </div>                                                                                                                                 
+                <div className="hover:opacity-80 cursor-pointer">
+                    <label className="flex justify-between cursor-pointer gap-3 font-bold text-[var(--l-text-light)]">
+                        Dark Mode
+                        <input className="toggle cursor-pointer" type="checkbox"></input>
+                    </label>
+                </div>
             </div>
         </div>
     );
